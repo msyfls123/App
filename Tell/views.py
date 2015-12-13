0110a1a77acbee9517ba06219e4a038c81d41306
msyfls123 <m18771058712@live.com>
2015-12-13 12:52:35 +0800

    BackUp/12/13

---
+++ Tell/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+from Tell.models import Item
+
+def hello(request,name):
+	number = len(name)
+	context = {}
+	context['dying'] = 1
+	context['name'] = name
+	context['num'] = number
+	context['item'] = Item.objects.all()
+	return render(request, 'hello.html', context)
+
+def bye(request):
+	return HttpResponse('Good Bye')
