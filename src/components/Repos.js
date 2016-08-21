edba76a53c9098847be00e45dd213e893bf24ed3
msyfls123 <m18771058712@live.com>
2016-08-21 12:59:25 +0800

    Test BrowserHistory

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,11 +1,17 @@
 import React, { Component, PropTypes } from 'react';
-import { Router, Route, hashHistory, Link, IndexLink } from 'react-router';
+import { Router, Route, browserHistory, Link, IndexLink } from 'react-router';
 
 export default class Repos extends Component{
+  onClick(e) {
+    browserHistory.push('/')
+  }
+
   render() {
     return <div>
       Hello Repos!
       <p>{this.props.params.name}</p>
+      <button onClick={this.onClick}> "Back" </button>
     </div>
+
   }
 }
