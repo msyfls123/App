157082e30d18ee60436a4695cf0bc65a1df506f6
msyfls123 <m18771058712@live.com>
2017-02-26 23:07:38 +0800

    test cursor

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ console.log('Ok')
 console.log(NODE_ENV)
 
 require('css/index.styl')
+
+document.querySelector('#box').addEventListener('click', function(e){
+  console.log('click')
+  document.querySelector('#box1').emit('focus')
+})
