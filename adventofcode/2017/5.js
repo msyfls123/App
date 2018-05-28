c1d3b250179339c9f3fe5ae7c17fdc3667b48a9d
msyfls123 <msyfls123@gmail.com>
2018-05-28 17:31:57 +0800

    adventofcode: 2017/5 part 1

---
+++ adventofcode/2017/5.js
@@ -0,0 +1,25 @@
+function walk(input) {
+  let step = 0, count = 0
+  const raw = input.slice()
+  while (step >= 0 && step < raw.length) {
+    const stepLength = raw[step]
+    count += 1
+    raw[step] += 1
+    step = step + stepLength
+  }
+
+  return count
+}
+
+function resolve(path = './5.txt') {
+  const fs = require('fs')
+  const input = fs
+    .readFileSync(path, { encoding: 'utf-8' })
+    .split('\n')
+    .map(k => k.trim())
+    .filter(e => e.length)
+    .map(k => parseInt(k, 10))
+  return walk(input)
+}
+
+module.exports = { resolve }
