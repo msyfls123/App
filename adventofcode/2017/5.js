ed4f03b30d5afa8cadc9fbc0c65e5775d6320b9d
msyfls123 <msyfls123@gmail.com>
2018-05-28 17:41:24 +0800

    adventofcode: 2017/5 part 2

--- adventofcode/2017/5.js
+++ adventofcode/2017/5.js
@@ -11,6 +11,19 @@ function walk(input) {
   return count
 }
 
+function walkStranger(input) {
+  let step = 0, count = 0
+  const raw = input.slice()
+  while (step >= 0 && step < raw.length) {
+    const stepLength = raw[step]
+    count += 1
+    raw[step] += raw[step] >= 3 ? -1 : 1
+    step = step + stepLength
+  }
+
+  return count
+}
+
 function resolve(path = './5.txt') {
   const fs = require('fs')
   const input = fs
@@ -19,7 +32,7 @@ function resolve(path = './5.txt') {
     .map(k => k.trim())
     .filter(e => e.length)
     .map(k => parseInt(k, 10))
-  return walk(input)
+  return [walk(input), walkStranger(input)]
 }
 
 module.exports = { resolve }
